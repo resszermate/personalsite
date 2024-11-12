9874c81fe26e9af3ed271eb48e342ee4266941ef
resszermate <materesszer2@gmail.com>
2024-11-12 20:12:46 +0100

    feature components ContactIcons added some icons for contact methods

--- personalsite/src/components/Contact.js
+++ personalsite/src/components/Contact.js
@@ -1,13 +1,21 @@
 import Card from './Card';
 import { FaDiscord } from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
 
 
 function openLink(link) {
     window.open(link);
 }
+
+function copyToClipboard(text) {
+    navigator.clipboard.copyToClipboard(text);
+    alert("Username copied!")
+}
+
 function Contact() {
     const contactIcons = [
-        < FaDiscord className="Discord" size="10px" color="black" onClick={() => openLink("")} />
+        < FaDiscord className="Discord" size="10px" color="black" onClick={() => copyToClipboard("Abrams124#2657")} />,
+        < CgMail className="Mail" size="10px" color="black" onClick={() => copyToClipboard("materesszer2@gmail.com") } />,
     ]
 
     return (
